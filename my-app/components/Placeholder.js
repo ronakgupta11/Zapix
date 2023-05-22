f26085ffcb743621515057e70e8a92da89bcc204
Ronak Gupta <mailtoronakgupta@gmail.com>
2023-05-22 17:45:55 +0530

    Update Placeholder.js

--- my-app/components/Placeholder.js
+++ my-app/components/Placeholder.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Placeholder = () => {
   return (
     <div>
-        <div role="status" class="max-w-sm animate-pulse">
+        <div role="status" class="max-w-sm animate-pulse my-4">
     <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
     <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px] mb-2.5"></div>
     <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
@@ -12,7 +12,7 @@ const Placeholder = () => {
     <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px]"></div>
     <span class="sr-only">Loading...</span>
 </div>
-<div role="status" class="max-w-sm animate-pulse">
+<div role="status" class="max-w-sm animate-pulse my-4">
     <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
     <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px] mb-2.5"></div>
     <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
@@ -21,7 +21,7 @@ const Placeholder = () => {
     <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px]"></div>
     <span class="sr-only">Loading...</span>
 </div>
-<div role="status" class="max-w-sm animate-pulse">
+<div role="status" class="max-w-sm animate-pulse my-4">
     <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
     <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px] mb-2.5"></div>
     <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
@@ -30,7 +30,7 @@ const Placeholder = () => {
     <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px]"></div>
     <span class="sr-only">Loading...</span>
 </div>
-<div role="status" class="max-w-sm animate-pulse">
+<div role="status" class="max-w-sm animate-pulse my-4">
     <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
     <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px] mb-2.5"></div>
     <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
@@ -39,6 +39,16 @@ const Placeholder = () => {
     <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px]"></div>
     <span class="sr-only">Loading...</span>
 </div>
+<div role="status" class="max-w-sm animate-pulse my-4">
+    <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
+    <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px] mb-2.5"></div>
+    <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
+    <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[330px] mb-2.5"></div>
+    <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[300px] mb-2.5"></div>
+    <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px]"></div>
+    <span class="sr-only">Loading...</span>
+</div>
+
     </div>
   )
 }
